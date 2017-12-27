31f2a2c7c1da9aae905ab21cf52fe4c3f5f01f31
Zahaby-magalix <33207093+Zahaby-magalix@users.noreply.github.com>
2017-12-27 03:36:49 +0200

    Create startup-prometheus.sh

---
+++ configs/openfaas/startup-prometheus.sh
@@ -0,0 +1,2 @@
+wget -P /etc/prometheus/ https://raw.githubusercontent.com/MagalixTechnologies/mgx-templates-configs/master/configs/openfaas/prometheus.yml
+wget -P /etc/prometheus/ https://raw.githubusercontent.com/MagalixTechnologies/mgx-templates-configs/master/configs/openfaas/alert.rules
