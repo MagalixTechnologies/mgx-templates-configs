ce1245b49f0d4fdf3938ed7655a25dc71922c7b9
Zahaby-magalix <33207093+Zahaby-magalix@users.noreply.github.com>
2017-12-27 03:33:39 +0200

    Update startup-prometheus.sh

--- configs/openfaas/startup-prometheus.sh
+++ configs/openfaas/startup-prometheus.sh
@@ -1,4 +1,6 @@
 start on startup
 task
+apt-get update
+apt-get install wget
 wget -P /etc/prometheus/ https://raw.githubusercontent.com/MagalixTechnologies/mgx-templates-configs/master/configs/openfaas/prometheus.yml
 wget -P /etc/prometheus/ https://raw.githubusercontent.com/MagalixTechnologies/mgx-templates-configs/master/configs/openfaas/alert.rules
