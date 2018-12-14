7182ae92c990e0bf17e3e1b720097d1224c5475d
zahaby <mohamed.elzahaby-c@magalix.com>
2018-12-14 16:21:30 +0200

    remove unneeded folders

--- configs/openfaas/startup-prometheus.sh
+++
@@ -1,3 +0,0 @@
-rm /etc/prometheus/prometheus.yml
-wget -P /etc/prometheus/ https://raw.githubusercontent.com/MagalixTechnologies/mgx-templates-configs/master/configs/openfaas/prometheus.yml
-wget -P /etc/prometheus/ https://raw.githubusercontent.com/MagalixTechnologies/mgx-templates-configs/master/configs/openfaas/alert.rules
